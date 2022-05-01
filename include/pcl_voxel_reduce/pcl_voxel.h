09dba0fd41ba0f5c6031a0101d1243545324727e
Vladimir <v.ivan@ed.ac.uk>
2022-05-01 02:18:28 +0100

    Initial commit

---
+++ include/pcl_voxel_reduce/pcl_voxel.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+
+#include <pcl/common/common.h>
+#include <pcl_ros/filters/voxel_grid.h>
+#include <pcl_ros/transforms.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+
+class PointCloudProcessor
+{
+public:
+    PointCloudProcessor();
+
+protected:
+    void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud);
+
+    ros::NodeHandle nh_;
+    ros::Subscriber sub_;
+    ros::Publisher cloud_pub_;
+    tf::TransformListener tf_listener_;
+
+    std::string world_name_;
+    sensor_msgs::PointCloud2 cloud_;
+    float grid_resolution_;
+    float box_min_x_;
+    float box_min_y_;
+    float box_min_z_;
+    float box_max_x_;
+    float box_max_y_;
+    float box_max_z_;
+};
---
+++ src/pcl_voxel.cpp
@@ -0,0 +1,64 @@
+#include <pcl_voxel_reduce/pcl_voxel.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/filters/crop_box.h>
+
+void PointCloudProcessor::cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud)
+{
+    if ((cloud->width * cloud->height) == 0)
+    {
+        return;  //return if the cloud is not dense!
+    }
+    {
+        // Concatenate
+        sensor_msgs::PointCloud2 tmp;
+        pcl_ros::transformPointCloud(world_name_, *cloud, tmp, tf_listener_);
+
+        // Downsample
+        pcl::PCLPointCloud2::Ptr cloud_filtered(new pcl::PCLPointCloud2());
+        pcl::PCLPointCloud2::Ptr cloud_reduced(new pcl::PCLPointCloud2());
+        pcl::PCLPointCloud2::Ptr pcl_cloud(new pcl::PCLPointCloud2());
+        pcl_conversions::toPCL(tmp, *pcl_cloud);
+
+        pcl::CropBox<pcl::PCLPointCloud2> crop;
+        crop.setInputCloud(pcl_cloud);
+        Eigen::Vector4f box_min(box_min_x_, box_min_y_, box_min_z_, 1.f);
+        Eigen::Vector4f box_max(box_max_x_, box_max_y_, box_max_z_, 1.f);
+        crop.setMax(box_max);
+        crop.setMin(box_min);
+        crop.filter(*cloud_filtered);
+
+        pcl::VoxelGrid<pcl::PCLPointCloud2> grid;
+        grid.setInputCloud(cloud_filtered);
+        grid.setLeafSize(grid_resolution_, grid_resolution_, grid_resolution_);
+        grid.filter(*cloud_reduced);
+
+        // Publish
+        pcl_conversions::fromPCL(*cloud_reduced, tmp);
+        tmp.header.frame_id = world_name_;
+        cloud_pub_.publish(tmp);
+    }
+}
+
+PointCloudProcessor::PointCloudProcessor() : nh_("~"), tf_listener_()
+{
+    nh_.param<std::string>("frame", world_name_, "base_link");
+    nh_.param<float>("resolution", grid_resolution_, 0.1f);
+    nh_.param<float>("min_x", box_min_x_, -10.0f);
+    nh_.param<float>("min_y", box_min_y_, -10.0f);
+    nh_.param<float>("min_z", box_min_z_, -10.0f);
+    nh_.param<float>("max_x", box_max_x_,  10.0f);
+    nh_.param<float>("max_y", box_max_y_,  10.0f);
+    nh_.param<float>("max_z", box_max_z_,  10.0f);
+
+    sub_ = nh_.subscribe("input", 30, &PointCloudProcessor::cloud_cb, this);
+    cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("output", 30, true);
+    ROS_INFO_STREAM("Point cloud processor started ...");
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "point_cloud_processor");
+    PointCloudProcessor pc;
+    ros::spin();
+    return 0;
+}
